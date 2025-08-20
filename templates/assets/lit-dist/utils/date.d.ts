1159238c720a4a8c71f9a910c3bbd28719968661
MuMoo <iscottt@163.com>
2025-08-20 20:22:39 +0800

    feat: 优化lit components

--- templates/assets/lit-dist/utils/date.d.ts
+++
@@ -1,4 +0,0 @@
-
-export declare function formatDatetime(date: string | Date | undefined | null): string;
-export declare function timeAgo(date: string | Date | undefined | null): string;
-//# sourceMappingURL=date.d.ts.map
